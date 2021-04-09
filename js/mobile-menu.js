dec137d25746228f48ab06ae25078ca03a0a0f6c
Olha Tarasova <tarasovao0505@gmail.com>
2021-04-09 18:52:16 +0300

    Начало работы с Hero

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,13 @@
-( ( )  =>  { 
-  const  menuBtnRef  =  document . querySelector ( "[data-menu-button]" ) ; 
-  const  mobileMenuRef  =  document . querySelector ( "[data-menu]" ) ; 
-  const  mobileBtnClose  =  document . querySelector ( "[данные -menu-close] " ) ;
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
 
-  menuBtnRef . addEventListener ( "клик" ,  ( )  =>  { 
-    mobileMenuRef . classList . toggle ( "is-open" ) ; 
-  } ) ;
+  menuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
 
-  mobileBtnClose . addEventListener ( "клик" ,  ( )  =>  { 
-    mobileMenuRef . classList . toggle ( "is-open" ) ; 
-  } ) ; 
-} ) ( ) ;
+  mobileBtnClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
